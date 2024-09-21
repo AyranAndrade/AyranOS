d97e8127449a6b824071bb7d051fa5d41a23f36f
Ayran <ayran.sampaio@loggi.com>
2024-09-21 01:25:50 -0300

    Make the kernel run correctly for the first time

--- kernel/main.c
+++ kernel/main.c
@@ -1,4 +1,14 @@
 void main(void) {
-    while (1) {
-    }
+    char *video_memory = (char *)0xb8000;
+    video_memory[0] = 'A';
+    video_memory[1] = 0x07;
+    video_memory[2] = 'Y';
+    video_memory[3] = 0x07;
+    video_memory[4] = 'R';
+    video_memory[5] = 0x07;
+    video_memory[6] = 'A';
+    video_memory[7] = 0x07;
+    video_memory[8] = 'N';
+
+    while(1) {}  
 }
