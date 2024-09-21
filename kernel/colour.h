8edb8d0417b5cf7d8dd6e4e11d27d9a8ca68a04f
Ayran <ayran.sampaio@loggi.com>
2024-09-21 18:46:20 -0300

    Add colour.h

---
+++ kernel/colour.h
@@ -0,0 +1,18 @@
+#define BLACK 0x0
+#define BLUE 0x1
+#define GREEN 0x2
+#define CYAN 0x3
+#define RED 0x4
+#define MAGENTA 0x5
+#define BROWN 0x6
+#define LIGHT_GRAY 0x7
+#define DARK_GRAY 0x8
+#define LIGHT_BLUE 0x9
+#define LIGHT_GREEN 0xA
+#define LIGHT_CYAN 0xB
+#define LIGHT_RED 0xC
+#define LIGHT_MAGENTA 0xD
+#define LIGHT_YELLOW 0xE
+#define BRIGHT_WHITE 0xF
+
+#define LIGHT_GRAY_ON_BLACK 0x07
--- kernel/screen.c
+++ kernel/screen.c
@@ -1,17 +1,18 @@
 #include "screen.h"
 #include "string.h"
 #include "constants.h"
+#include "colour.h"
 
 void print(char* value) {
     for (int i = 0; i < getLength(value); i++) {
         VIDEO_MEMORY[i * 2] = value[i];
-        VIDEO_MEMORY[i * 2 + 1] = WHITE; 
+        VIDEO_MEMORY[i * 2 + 1] = LIGHT_GRAY_ON_BLACK; 
     }
 }
 
 void clearScreen() {
     for (int i = 0; i < NUMBER_OF_CHARACTERS; i++) {
         VIDEO_MEMORY[i * 2] = ' ';
-        VIDEO_MEMORY[i * 2 + 1] = WHITE; 
+        VIDEO_MEMORY[i * 2 + 1] = LIGHT_GRAY_ON_BLACK; 
     }
 }
